5224f6d77f8e8615ed158776b97fd8ab867d068a
starry69 <starry369126@outlook.com>
2020-08-28 21:50:29 +0530

    Federations: Add /renamefed command
    Signed-off-by: starry69 <starry369126@outlook.com>

--- miss_evie/modules/sql/feds_sql.py
+++ miss_evie/modules/sql/feds_sql.py
@@ -734,6 +734,26 @@ def unsubs_fed(fed_id, my_fed):
         return False
 
 
+def rename_fed(fed_id, owner_id, newname):
+    with FEDS_LOCK:
+        global FEDERATION_BYFEDID, FEDERATION_BYOWNER, FEDERATION_BYNAME
+        fed = SESSION.query(Federations).get(fed_id)
+        if not fed:
+            return False
+        fed.fed_name = newname
+        SESSION.commit()
+
+        # Update the dicts
+        oldname = FEDERATION_BYFEDID[str(fed_id)]["fname"]
+        tempdata = FEDERATION_BYNAME[oldname]
+        FEDERATION_BYNAME.pop(oldname)
+
+        FEDERATION_BYOWNER[str(owner_id)]["fname"] = newname
+        FEDERATION_BYFEDID[str(fed_id)]["fname"] = newname
+        FEDERATION_BYNAME[newname] = tempdata
+        return True
+
+
 def get_all_subs(fed_id):
     return FEDS_SUBSCRIBER.get(fed_id, set())
 
@@ -747,7 +767,7 @@ def get_spec_subs(fed_id, fed_target):
 
 def get_mysubs(my_fed):
     if MYFEDS_SUBSCRIBER.get(my_fed) is None:
-       return None
+        return None
     return list(MYFEDS_SUBSCRIBER.get(my_fed))
 
 
